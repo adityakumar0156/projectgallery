0fb9581badeb9df9e4c3ba47b6c35649583a5df2
Aditya Kumar <73734281+adityakumar0156@users.noreply.github.com>
2021-12-30 16:36:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express();
+const hbs = require('hbs');
+const path = require('path')
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use('/static', express.static('public'));
+
+// Simply app.use means “Run this on ALL requests” app.get
+//  means “Run this on a GET request, for the given URL”
+app.get('/', (req, res) => {
+    res.render('home')
+        // res.sendFile(path.join(__dirname, '/home.html'));
+})
+app.get('/chatapp', (req, res) => {
+    res.render('chatapp')
+        // res.sendFile(path.join(__dirname, '/chatapp.html'));
+
+})
+
+
+
+app.listen(8000, () => {
+    console.log("App is listening on port 8000");
+})
---
+++ public/client.js
@@ -0,0 +1,8 @@
+console.log("jai Shree Ram!!");
+const fun1 = () => {
+    document.getElementById('my_btn').click();
+}
+
+const fun2 = () => {
+    document.getElementById('my_btn2').click();
+}
